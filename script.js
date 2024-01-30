08182832ac7ed056c1d396ae82018cbd86269dbc
Vishnu Vardhan Vankayalapati <vishnuvankayalapati@giggi.io>
2024-01-30 02:35:31 -0500

    portfolio

--- script.js
+++
@@ -1,6 +0,0 @@
-function foo(name, email, message) {
-    mail_text = "You have a mail from " + name + " their email address is: " + email.value + " Here's what they wrote" + message.value;
-    subject = "Inquiry";
-    window.open('mailto:vankayalapativ4@gmail.com?subject=subject&body=mail_text');
-    return true;
- }
